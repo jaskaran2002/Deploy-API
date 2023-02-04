e3f1388c2db968c1b6d9c794f95acf135cc1cc9a
Jaskaran Singh <jas2002karan@gmail.com>
2023-02-04 14:33:42 +0530

    aaaaaaa

--- sherlock/sherlock/sherlock.py
+++ sherlock/sherlock/sherlock.py
@@ -463,15 +463,15 @@ def timeout_check(value):
 
     NOTE:  Will raise an exception if the timeout in invalid.
     """
-    from argparse import ArgumentTypeError
+    # from argparse import ArgumentTypeError
 
-    try:
-        timeout = float(value)
-    except:
-        raise ArgumentTypeError(f"Timeout '{value}' must be a number.")
-    if timeout <= 0:
-        raise ArgumentTypeError(
-            f"Timeout '{value}' must be greater than 0.0s.")
+    # try:
+    timeout = float(value)
+    # except:
+    #     raise ArgumentTypeError(f"Timeout '{value}' must be a number.")
+    # if timeout <= 0:
+    #     raise ArgumentTypeError(
+    #         f"Timeout '{value}' must be greater than 0.0s.")
     return timeout
 
 
